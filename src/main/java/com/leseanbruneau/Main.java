bc6c2fb9da6dcad2bd530daacac8a141de6e2244
hogy <hogy@hogy-VirtualBox>
2020-05-13 07:22:18 -0500

    Section 6 - Completed

--- src/main/java/com/leseanbruneau/Main.java
+++ src/main/java/com/leseanbruneau/Main.java
@@ -6,8 +6,11 @@ import java.util.List;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+
+import com.google.common.collect.Iterables;
 
 import scala.Tuple2;
 
@@ -15,21 +18,56 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		List<Integer> inputData = new ArrayList<>();
-		inputData.add(35);
-		inputData.add(12);
-		inputData.add(90);
-		inputData.add(20);
+		List<String> inputData = new ArrayList<>();
+		inputData.add("WARN:  Tuesday 4 September 0405");
+		inputData.add("WARN:  Tuesday 4 September 0406");
+		inputData.add("ERROR:  Tuesday 4 September 0408");
+		inputData.add("FATAL:  Wednesday 5 September 1632");
+		inputData.add("ERROR:  Friday 7 September 1854");
+		inputData.add("WARN:  Saturday 8 September 1942");
 		
 		Logger.getLogger("org.apache").setLevel(Level.WARN);
 		
 		SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		
-		JavaRDD<Integer> originalIntegers = sc.parallelize(inputData);
-		JavaRDD<Tuple2<Integer, Double>> sqrtRdd = originalIntegers.map( value -> new Tuple2<>(value, Math.sqrt(value)) );
-
-				
+		/*
+		JavaRDD<String> originalLogMessages = sc.parallelize(inputData);
+		
+		//JavaPairRDD<String, String> pairRdd = originalLogMessages.mapToPair( rawValue -> {
+		JavaPairRDD<String, Long> pairRdd = originalLogMessages.mapToPair( rawValue -> {
+			
+			String[] columns = rawValue.split(":");
+			String level = columns[0];
+			String date = columns[1];
+			
+			//return new Tuple2<String, String>(level, date);
+			// JavePairRDD specifies types <String, String>, so can remove
+			return new Tuple2<>(level, 1L);
+			
+		} );
+		
+		JavaPairRDD<String, Long> sumsRdd = pairRdd.reduceByKey( (value1, value2) -> value1 + value2);
+		
+		sumsRdd.foreach( tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+		*/
+		
+//		sc.parallelize(inputData)
+//		  .mapToPair( rawValue -> new Tuple2<>(rawValue.split(":")[0] , 1L ))
+//		  .reduceByKey((value1, value2) -> value1 + value2)
+//		  .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+		
+		// groupbykey version
+		
+		sc.parallelize(inputData)
+		  .mapToPair( rawValue -> new Tuple2<>(rawValue.split(":")[0] , 1L ))
+		  .groupByKey()
+		  .foreach( tuple -> System.out.println(tuple._1 + " has " + Iterables.size(tuple._2) + " instances "));
+		
+		// NOTE:
+		// 
+		//  Use reduceByKey whenever possible - better performance
+		
 		sc.close();
 
 	}
