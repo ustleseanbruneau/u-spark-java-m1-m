2c8d17782e19c71fba25243cc06e6265da5e9820
hogy <hogy@hogy-VirtualBox>
2020-05-13 07:52:11 -0500

    Section 7 - Completed

--- src/main/java/com/leseanbruneau/Main.java
+++ src/main/java/com/leseanbruneau/Main.java
@@ -1,6 +1,7 @@
 package com.leseanbruneau;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.Level;
@@ -31,42 +32,19 @@ public class Main {
 		SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		
-		/*
-		JavaRDD<String> originalLogMessages = sc.parallelize(inputData);
+//		JavaRDD<String> sentences = sc.parallelize(inputData);
+//		
+//		JavaRDD<String> words = sentences.flatMap(value -> Arrays.asList(value.split(" ")).iterator());
+//		
+//		JavaRDD<String> filteredWords = words.filter(word -> word.length() > 1);
+//		
+//		filteredWords.collect().forEach(System.out::println);
 		
-		//JavaPairRDD<String, String> pairRdd = originalLogMessages.mapToPair( rawValue -> {
-		JavaPairRDD<String, Long> pairRdd = originalLogMessages.mapToPair( rawValue -> {
-			
-			String[] columns = rawValue.split(":");
-			String level = columns[0];
-			String date = columns[1];
-			
-			//return new Tuple2<String, String>(level, date);
-			// JavePairRDD specifies types <String, String>, so can remove
-			return new Tuple2<>(level, 1L);
-			
-		} );
-		
-		JavaPairRDD<String, Long> sumsRdd = pairRdd.reduceByKey( (value1, value2) -> value1 + value2);
-		
-		sumsRdd.foreach( tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
-		*/
-		
-//		sc.parallelize(inputData)
-//		  .mapToPair( rawValue -> new Tuple2<>(rawValue.split(":")[0] , 1L ))
-//		  .reduceByKey((value1, value2) -> value1 + value2)
-//		  .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
-		
-		// groupbykey version
 		
 		sc.parallelize(inputData)
-		  .mapToPair( rawValue -> new Tuple2<>(rawValue.split(":")[0] , 1L ))
-		  .groupByKey()
-		  .foreach( tuple -> System.out.println(tuple._1 + " has " + Iterables.size(tuple._2) + " instances "));
-		
-		// NOTE:
-		// 
-		//  Use reduceByKey whenever possible - better performance
+		  .flatMap(value -> Arrays.asList(value.split(" ")).iterator())
+		  .filter(word -> word.length() > 1)
+		  .collect().forEach(System.out::println);
 		
 		sc.close();
 
